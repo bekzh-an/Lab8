0b6df0f622aa65f68b1925897287fe044b803f31
Bekzhan <bagliby725@gmail.com>
2026-05-25 19:15:28 -0700

    update sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - This file needs to be in the root of the directory to work,
-//         so do not move it next to the other scripts
+//         so do not move it next to the other scripts.
 
 const CACHE_NAME = 'lab-8-starter';
 
